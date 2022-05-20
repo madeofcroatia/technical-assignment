8af2ab8e4b5a62d00e8d4f5936cfe9034e9e6117
madeofcroatia <52186621+madeofcroatia@users.noreply.github.com>
2022-05-20 22:04:43 +0500

    moved text messages from main.py to responses.py

---
+++ messages/responses.py
@@ -0,0 +1,13 @@
+email_message = "Subject: Reservation Confirmation\n" \
+                "Hi {},\n\n" \
+                "Your reservation for room number {} was successful!\n\n" \
+                "It starts on {} at {} and lasts until {} at {}!\n\n" \
+                "Best regards,\n\n" \
+                "Reservations Manager"
+email_sent = "Email confirmation was sent! If you are done, press Ctrl+C on your keyboard " \
+             "and if you would like to check availability for another room or make another" \
+             " reservation, stay with us!"
+res_success = "Reservation was successful! Sending email confirmation..."
+room_always_busy = "Sorry, but the room that you are trying to book is not available at all."
+room_busy = "Sorry, but this room is busy in the given timeframe! The next time it will be available for the same" \
+            " duration is on {} at {}. Would you like to book it for that timeframe?(y/n) "
